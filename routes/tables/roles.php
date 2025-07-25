15959f769520e938bcbde5131e68d678700fc3c4
migueldev2006 <gironorozcoluismiguel3@gmail.com>
2025-07-25 15:32:08 -0500

    roles

--- routes/tables/roles.php
+++ routes/tables/roles.php
@@ -1,4 +1,11 @@
 <?php
 
+use App\Http\Controllers\RolesController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Roles;
+
+Route::get('roles',[RolesController::class, 'index']);
+Route::get('roles/{id_rol}',[RolesController::class, 'show']);
+Route::post('roles',[RolesController::class, 'store']);
+Route::patch('roles/{id_rol}',[RolesController::class, 'update']);
+Route::delete('roles/state/{id_rol}',[RolesController::class, 'destroy']);
